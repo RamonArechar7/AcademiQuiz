bdbaca6178fe672aa35b9b01b3fef5a0b6ee365a
ramon <1224100505.rar@gmail.com>
2025-12-05 15:08:30 -0600

    README actualizado, gitignore actualizado y capturas de pantalla incluidas.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,4 +92,9 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
